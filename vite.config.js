357d6111bba1b113dc1bf62e7575e74b67547a41
Samuel FOTSO <126283225+samitochi04@users.noreply.github.com>
2025-06-21 19:55:07 +0200

    Update vite.config.js issa

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,4 +9,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  preview: {
+    port: 4173,
+    host: true,
+    allowedHosts: ['qksgkso8ws00k440k0o0w4ss.31.97.68.94.sslip.io']
+  }
 })
