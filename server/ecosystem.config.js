7f5e4078fac44050a1390a00ebd603fa7e5fe7ac
samuel <temmodaryl317@gmail.com>
2025-06-22 11:17:51 +0200

    deploying back end --Remove Docker Config

---
+++ server/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'cameroon-marketplace-api',
+    script: 'index.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '512M',
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    error_file: 'logs/err.log',
+    out_file: 'logs/out.log',
+    time: true
+  }]
+}
